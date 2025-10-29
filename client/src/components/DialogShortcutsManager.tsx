ae469d7a92deff30b2ce3112ea1e599fac6500b6
rikiMaman <148782616+rikiMaman@users.noreply.github.com>
2025-10-29 10:33:27 +0200

    version 2 useKeyBoardShortcuts

--- client/src/components/DialogShortcutsManager.tsx
+++ client/src/components/DialogShortcutsManager.tsx
@@ -3,30 +3,31 @@
 import React from "react";
 import { useKeyboardShortcuts } from "../app/hooks/useKeyboardShortcuts";
 
-type DialogShortcutsManagerProps = {
-  onOpenHelp: () => void;
-  onOpenConfirm: () => void;
-  onOpenResults: () => void;
-  onOpenToast: () => void;
-};
+// type DialogShortcutsManagerProps = {
+//   onOpenHelp: () => void;
+//   onOpenConfirm: () => void;
+//   onOpenResults: () => void;
+//   onOpenToast: () => void;
+// };
 
 export default function DialogShortcutsManager({
   onOpenHelp,
   onOpenConfirm,
   onOpenResults,
   onOpenToast,
-}: DialogShortcutsManagerProps) {
-  // קיצור H - עזרה
-  useKeyboardShortcuts(["h", "H"], onOpenHelp);
+}:
+//  DialogShortcutsManagerProps) {
+//   // קיצור H - עזרה
+//   useKeyboardShortcuts(["h", "H"], onOpenHelp);
 
-  // קיצור C - אישור
-  useKeyboardShortcuts(["c", "C"], onOpenConfirm);
+//   // קיצור C - אישור
+//   useKeyboardShortcuts(["c", "C"], onOpenConfirm);
 
-  // קיצור R - תוצאות
-  useKeyboardShortcuts(["r", "R"], onOpenResults);
+//   // קיצור R - תוצאות
+//   useKeyboardShortcuts(["r", "R"], onOpenResults);
 
-  // קיצור T - הודעה
-  useKeyboardShortcuts(["t", "T"], onOpenToast);
+//   // קיצור T - הודעה
+//   useKeyboardShortcuts(["t", "T"], onOpenToast);
 
-  return null; // לא מציגה שום דבר על המסך
-}
+//   return null; // לא מציגה שום דבר על המסך
+// }
